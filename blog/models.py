8052b64ab0a96dcadf1c364761c1a14066790661
Yancel M. Salinas Gomez <sagoyanfisic1@gmail.com>
2015-08-15 03:57:22 -0500

    models update type community
    update community

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     evento_activo =models.BooleanField(default=False)
     fecha_publicacion = models.DateTimeField(auto_now=True)
     titulo = models.CharField(max_length=200)
-    #banner =models.ImageField(upload_to='fotos', verbose_name='Banner')
-    #banner_grande =models.ImageField(upload_to='fotos', verbose_name='Flyer')
+    banner =models.ImageField(upload_to='static/upload/', verbose_name='Banner')
+    banner_grande =models.ImageField(upload_to='static/upload/', verbose_name='Flyer')
     contenido = models.TextField(max_length=200)
     fecha_evento = models.DateTimeField(default=timezone.now())
     lugar =models.CharField(max_length=100)
@@ -15,6 +16,7 @@ class Post(models.Model):
     capacidad = models.IntegerField(max_length=4)
     tema = models.CharField(max_length=10)
     expositor = models.CharField(max_length=50)
+    usuario = models.ForeignKey(User)
 
 
     def __unicode__(self):
@@ -26,9 +28,14 @@ class Miembros(models.Model):
      miembro_activo = models.BooleanField(default=True)
      nombre = models.CharField(max_length=50)
      apellido = models.CharField(max_length=60)
-     #perfil=models.ImageField(upload_to='avatar',verbose_name='Foto de perfil')
+     perfil=models.ImageField(upload_to='static/upload/',verbose_name='Foto de perfil')
      perfil_json=models.URLField(max_length=200,blank=True, null= False)
      correo =models.EmailField(max_length=70,blank=True, null= True, unique= True)
+     COMUNIDAD_UIGV = (
+        ('CS','COSOLIG'),
+        ('CH','CHIAS-GRUPO ROBOTICA'),
+        )
+     tipo =models.CharField(max_length=6,choices=COMUNIDAD_UIGV)
 
      TYPE_CHOICES = (
        ('PR', 'Presidente'),
